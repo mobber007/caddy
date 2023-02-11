e731440d3c7fc2bf1c8a5baef2532110e79b25e0
mobber007 <thefactsmob@gmail.com>
2023-02-11 22:17:13 +0200

    fdsa

---
+++ box.sh
@@ -0,0 +1,19 @@
+sftp_user="cafelutsa"
+sftp_password="meleanca1993"
+sftp_group="box"
+site="cafelutsa.ro"
+home_dir="/home/$sftp_user"
+box_dir="$home_dir/box"
+public_dir="$box_dir/$site/public"
+
+useradd $sftp_user
+mkdir -p $public_dir
+echo -e "$sftp_password\n$sftp_password" | passwd $sftp_user
+chown -R $sftp_user:$sftp_user $home_dir
+adduser $sftp_user $sftp_group
+usermod -s /bin/false $sftp_user
+chown root:root $home_dir
+chown -R $sftp_user:$sftp_user $box_dir
+chmod 755 $box_dir
+sudo -u $sftp_user curl -s https://raw.githubusercontent.com/mobber007/caddy/main/index.html -o $public_dir/index.html
+caddy file-server --root $public_dir --domain $site
---
+++ sftp.sh
@@ -0,0 +1,25 @@
+sftp_group="box"
+ssh_config_file="/etc/ssh/sshd_config"
+
+sed -i "/Subsystem/c\Subsystem sftp internal-sftp" $ssh_config_file
+
+if ! grep -q "Match Group $sftp_group" $ssh_config_file
+then
+    cat <<EOT >> $ssh_config_file
+Match Group $sftp_group
+ChrootDirectory /home/%u/sites
+ForceCommand internal-sftp
+PasswordAuthentication yes
+PermitTunnel no
+AllowAgentForwarding no
+AllowTcpForwarding no
+X11Forwarding no
+EOT
+
+fi
+
+if ! grep -q $sftp_group "/etc/group"
+then
+    addgroup $sftp_group
+fi
+service ssh restart
