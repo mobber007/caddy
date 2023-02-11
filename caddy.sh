1ded7e85bf4f03c8c8c0ca0cc8677d5d0b1bf720
mobber007 <thefactsmob@gmail.com>
2023-02-11 20:23:25 +0200

    kewl

--- caddy.sh
+++ caddy.sh
@@ -3,6 +3,8 @@ curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmo
 curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
 apt update -y
 apt install -y caddy jq curl wget zip unzip
+caddy stop
 caddy start
 curl -s https://raw.githubusercontent.com/mobber007/caddy/main/caddy.json -o caddy.json
 curl localhost:2019/load -H "Content-Type: application/json" -d @caddy.json
+caddy reload
