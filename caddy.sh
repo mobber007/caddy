347da88e40f7802559ed11221ecc560c4de175a3
mobber007 <thefactsmob@gmail.com>
2023-02-11 20:16:55 +0200

    2

--- caddy.sh
+++ caddy.sh
@@ -1,6 +1,8 @@
-sudo apt install -y debian-keyring debian-archive-keyring apt-transport-https
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | sudo gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
-curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | sudo tee /etc/apt/sources.list.d/caddy-stable.list
-sudo apt update
-sudo apt install caddy
-curl -s https://mele.lol/caddy.json -o caddy.json
+apt install -y debian-keyring debian-archive-keyring apt-transport-https
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/gpg.key' | gpg --dearmor -o /usr/share/keyrings/caddy-stable-archive-keyring.gpg
+curl -1sLf 'https://dl.cloudsmith.io/public/caddy/stable/debian.deb.txt' | tee /etc/apt/sources.list.d/caddy-stable.list
+apt update -y
+apt install -y caddy jq curl wget zip unzip
+caddy start
+curl -s https://raw.githubusercontent.com/mobber007/caddy/main/caddy.json -o caddy.json
+curl localhost:2019/load -H "Content-Type: application/json" -d @caddy.json
